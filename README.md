5a1d8ed1e6a9b216b5d4f8991d1b8ab324a0510d
Jack Davidson <jackmaverickdavidson@gmail.com>
2021-05-03 13:08:49 -0500

    Added license, cleaned up vcping.py, and added a readme.

---
+++ README.md
@@ -0,0 +1,7 @@
+# vcping discord bot
+
+## files
+`token`: place token in this file, this will be loaded by vcping.py to authenticate
+with discord.
+
+`vcping.py`: primary executable for vcping.
--- vcping.py
+++ vcping.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python3
 import discord
 
 client = discord.Client()
 
+
 def get_role(guild, name):
     return discord.utils.get(guild.roles, name=name)
+
 
 @client.event
 async def on_voice_state_update(member, before, after):
@@ -18,7 +21,9 @@ async def on_voice_state_update(member, before, after):
         await member.add_roles(role)
         print("giving member " + member.name + " role " + role.name)
     if before.channel:
-        print("removing role " + get_role(guild, before.channel.name).name + " from member " + member.name)
+        print("removing role "
+              + get_role(guild, before.channel.name).name
+              + " from member " + member.name)
         await member.remove_roles(get_role(guild, before.channel.name))
 
 client.run('ODM0MjYzMjkxOTY5Nzk4MTY0.YH-WPA.8KjR4og2bBYJry5DxgQDmz3myOI')
